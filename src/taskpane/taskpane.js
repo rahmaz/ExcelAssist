6f57b88886357a83491e1e23f79e4e6e3dda39eb
Rahma <azamihassanirahma@gmail.com>
2021-11-12 08:15:17 +0100

    more commands not tested

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -8,14 +8,41 @@ Office.onReady((info) => {
 });
 
 export async function executeCommand() {
-  const commandVal = document.getElementById("command").value;
-  //console.log("commandVal=====", commandVal);
-  var parsedCommand = commandVal.split(" ");
-  //console.log("parsedCommand=====", parsedCommand);
-  var verb = parsedCommand[0];
-  //console.log("Verb=====", verb);
+  const commandVal = document.getElementById("command").value; //get the user input text
+  const commandLow = commandVal.toLowerCase(); //LowerCase
+  var parsedCommand = commandLow.split(" "); //split the commands into words
+  var verb = parsedCommand[0]; //get the first word to know which command to execute
+  var verbnext = parsedCommand[1]; //if next word is needed ex: move screen
+  var verblast = parsedCommand[parsedCommand.length]; //get the last word in case verb is last
+  //depending on the verb, execute one of the following functions
+  //Group 1
   if (verb == "sort") sortCommand(parsedCommand);
   else if (verb == "swap") swapCommand(parsedCommand);
+  //Group 2
+  else if (verb == "clear") clearCmd(parsedCommand);
+  else if (verb == "delete") deleteCmd(parsedCommand);
+  else if (verb == "copy") copyCmd(parsedCommand);
+  else if (verb == "highlight") highlightCmd(parsedCommand);
+  else if (verb == "move" && (verbnext == "cell" || verbnext == "column" || verbnext == "row" || verbnext == "table"))
+    moveCmd(parsedCommand);
+  else if (verblast == "bold") boldCmd(parsedCommand);
+  else if (verb == "adjust") adjustCmd(parsedCommand);
+  //Group 3
+  else if (verb == "paste") pasteCmd(parsedCommand);
+  //Group 4
+  else if (verb == "revert") revertCmd(parsedCommand);
+  else if (verb == "save") saveCmd(parsedCommand);
+  //Group 5
+  else if (verb == "zoom") zoomCmd(parsedCommand);
+  else if (verb == "move" && (verbnext == "screen" || verbnext == "sheet")) moveScreenCmd(parsedCommand);
+  //Group 6
+  else if (verb == "sum" || verb == "add") sumCmd(parsedCommand);
+  else if (verb == "average") avgCmd(parsedCommand);
+  else if (verblast == "fraction") fractionCmd(parsedCommand);
+  else if (verblast == "decimal") decimalCmd(parsedCommand);
+  //Group 7
+  else if (verb == "hide") hideCmd(parsedCommand);
+  else if (verb == "insert") insertCmd(parsedCommand);
   return context.sync();
 }
 
@@ -29,14 +56,14 @@ export async function sortCommand(parsedCommand) {
   var myRow;
   //checking if it's sort column or sort row, if row is not mentioned it assumes it's column
   for (var k = 0; k < commandLength; k++) {
-    if (parsedCommand[k] == "row" || parsedCommand[k] == "Row") {
+    if (parsedCommand[k] == "row") {
       RC = 0;
     }
   }
   //Get Column Name
   if (RC == 1) {
     for (var i = 0; i < commandLength; i++) {
-      if (parsedCommand[i] == "column" || parsedCommand[i] == "Column") {
+      if (parsedCommand[i] == "column") {
         myColumn = parsedCommand[i + 1];
         //TO DO: handle case where column name is mentioned before the word column
         //TO DO: handle all types of arguments
@@ -44,7 +71,7 @@ export async function sortCommand(parsedCommand) {
     }
   } /* Get Row Name */ else {
     for (var r = 0; r < commandLength; r++) {
-      if (parsedCommand[r] == "row" || parsedCommand[i] == "Row") {
+      if (parsedCommand[r] == "row") {
         myRow = parsedCommand[i + 1];
         //TO DO: handle case where row name is mentioned before the word row
         //TO DO: handle all types of arguments
@@ -55,7 +82,7 @@ export async function sortCommand(parsedCommand) {
   var OrderAsc; //true is ascending
   for (var j = 0; j < commandLength; j++) {
     /* Not sure if includes() will work */
-    if (parsedCommand[j].includes("Desc") == true || parsedCommand[j].includes("desc") == true) {
+    if (parsedCommand[j].includes("desc") == true) {
       OrderAsc = false;
       //TO DO: handle spelling mistakes
     }
@@ -90,14 +117,14 @@ export async function swapCommand(parsedCommand) {
   var myRow2;
   //checking if it's sort column or sort row, if row is not mentioned it assumes it's column
   for (var k = 0; k < commandLength; k++) {
-    if (parsedCommand[k] == "rows" || parsedCommand[k] == "Rows" || parsedCommand[k] == "row") {
+    if (parsedCommand[k] == "rows" || parsedCommand[k] == "row") {
       RC = 0;
     }
   }
   //Get Column Names
   if (RC == 1) {
     for (var i = 0; i < commandLength; i++) {
-      if (parsedCommand[i] == "columns" || parsedCommand[i] == "Columns" || parsedCommand[i] == "column") {
+      if (parsedCommand[i] == "columns" || parsedCommand[i] == "column") {
         myColumn1 = parsedCommand[i + 1];
         if (parsedCommand[i + 2] == "and") myColumn2 = parsedCommand[i + 3];
         else myColumn2 = parsedCommand[i + 2];
@@ -107,7 +134,7 @@ export async function swapCommand(parsedCommand) {
     }
   } /* Get Row Names */ else {
     for (var r = 0; r < commandLength; r++) {
-      if (parsedCommand[r] == "rows" || parsedCommand[i] == "Rows" || parsedCommand[k] == "row") {
+      if (parsedCommand[r] == "rows" || parsedCommand[k] == "row") {
         myRow1 = parsedCommand[i + 1];
         if (parsedCommand[r + 2] == "and") myRow2 = parsedCommand[r + 3];
         else myRow2 = parsedCommand[i + 2];
@@ -122,7 +149,83 @@ export async function swapCommand(parsedCommand) {
     var sheet = context.workbook.worksheets.getActiveWorksheet();
     var myTable = sheet.tables.getItem("test");
     var columnRange = myTable.getDataBodyRange();
-    columnRange.moveColumnByIndex();
+    //columnRange.moveColumnByIndex(,);
     //either move the columns and store or traditional copy temp
   });
+}
+
+export async function clearCmd(parsedCommand) {
+  var sheet = context.workbook.worksheets.getActiveWorksheet();
+  var range;
+  if (parsedCommand[1] == "table") {
+    //get table range
+    range = sheet.getRange("X");
+    range.clear();
+  } else if (parsedCommand[1] == "column") {
+    //get column range
+    range = sheet.getRange("X");
+    range.clear();
+  } else if (parsedCommand[1] == "row") {
+    //get row range
+    range = sheet.getRange("X");
+    range.clear();
+  } else if (parsedCommand[1] == "cell") {
+    //get cell range
+    range = sheet.getRange("X");
+    range.clear();
+  } else if (parsedCommand[1] == "sheet") {
+    range = sheet.getUsedRange(); //gets the smallest range that encompasses any cells in the worksheet that have a value or formatting assigned to them
+    range.clear();
+  }
+  return context.sync();
+}
+
+export async function deleteCmd(parsedCommand) {
+  var sheet = context.workbook.worksheets.getActiveWorksheet();
+  var range;
+  if (parsedCommand[1] == "table") {
+    //get table range
+    range = sheet.getRange("X");
+    range.clear();
+  } else if (parsedCommand[1] == "column") {
+    //get column range
+    range = sheet.getRange("X");
+    range.delete(Excel.DeleteShiftDirection.left);
+  } else if (parsedCommand[1] == "row") {
+    //get row range
+    range = sheet.getRange("X");
+    range.delete(Excel.DeleteShiftDirection.up);
+  } else if (parsedCommand[1] == "cell") {
+    //get cell range
+    range = sheet.getRange("X");
+    range.clear();
+  } else if (parsedCommand[1] == "sheet") {
+    //delete the whole worksheet
+    sheet.removeWorksheet(sheet.id);
+  }
+  return context.sync();
+}
+
+export async function copyPasteCmd(parsedCommand) {
+  var sheet = context.workbook.worksheets.getActiveWorksheet();
+  var range;
+  var rangePaste;
+  if (parsedCommand[1] == "table") {
+    //get table range
+    range = sheet.getRange("X");
+    range.copyFrom(rangePaste);
+  } else if (parsedCommand[1] == "column") {
+    //get column range
+    range = sheet.getRange("X");
+    range.copyFrom(rangePaste);
+  } else if (parsedCommand[1] == "row") {
+    //get row range
+    range = sheet.getRange("X");
+    range.copyFrom(rangePaste);
+  } else if (parsedCommand[1] == "cell") {
+    //get cell range
+    range = sheet.getRange("X");
+    range.copyFrom(rangePaste);
+  }
+  return context.sync();
 }
